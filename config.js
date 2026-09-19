942daadfa0809d63906234b9152af4313ddff8d7
MRDEVIL786143 <rohakhan777777@gmail.com>
2026-09-19 07:33:15 +0000

    Extract project files from archive

---
+++ config.js
@@ -0,0 +1,6 @@
+global.owner = ['628XXXXX'] // owner number
+global.connect = true // Ubah ke false jika ingin mengkoneksikan bot menggunakan QrCode
+global.url = "https://t.me/IkramOffcial"
+global.url2 = "https://t.me/IkramIsHere"
+global.packname = "𝗧𝗿𝗮𝘃𝗮𝘀 𝗗𝗵𝗲𝗮𝘁𝗵"
+global.author = "IkramOfficial"
